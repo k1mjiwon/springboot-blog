73222396fe7e451639ee4614f65cd745b34d943b
k1mjiwon <km_jw@naver.com>
2024-10-28 17:39:23 +0900

    add delete func

--- src/main/java/me/jiwon/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/me/jiwon/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,15 @@
 package me.jiwon.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.jiwon.springbootdeveloper.domain.Article;
 import me.jiwon.springbootdeveloper.dto.ArticleListViewResponse;
+import me.jiwon.springbootdeveloper.dto.ArticleViewResponse;
 import me.jiwon.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,5 +25,23 @@ public class BlogViewController {
                 .toList();
         model.addAttribute("articles", articles);
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable(name = "id") long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
