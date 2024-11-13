698be5b79122abc8fd8bf40da93235358bd610f1
k1mjiwon <km_jw@naver.com>
2024-11-13 17:12:47 +0900

    Jwt last commit

--- src/main/java/me/jiwon/springbootdeveloper/domain/RefreshToken.java
+++ src/main/java/me/jiwon/springbootdeveloper/domain/RefreshToken.java
@@ -17,5 +17,17 @@ public class RefreshToken {
     @Column(name = "user_id", nullable = false, unique = true)
     private Long userId;
 
-    //@Column(name = "refresh_token", nullable = false)
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+
 }
